cdbc2d197b8027a2e23504edffa4d60d4a06974c
Alex Angelini <alex.louis.angelini@gmail.com>
2015-01-02 23:48:37 -0500

    WIP: Refactor serf to line up messages.

--- src/exdb/serf.clj
+++ src/exdb/serf.clj
@@ -14,61 +14,96 @@
           (seq acc))))
     (catch SocketTimeoutException e nil)))
 
-(defn- write-socket [s b]
-  (.write (.getOutputStream s) b 0 (count b)))
+(defn- update-count [current-count new-count outputs]
+  (dosync
+   (let [current @current-count
+         keep? (:keep? (get outputs current))]
+     (alter current-count (fn [old new] new) new-count)
+     (when (not keep?)
+       (alter outputs dissoc current)))))
 
-(defn- read-messages [s]
-  (let [in (.getInputStream s)
-        chan (async/chan 10)]
+(defn- read-messages [{socket :socket outputs :outputs}]
+  (let [in (.getInputStream socket)
+        current-count (ref 0)]
     (async/go
       (loop [messages (read-socket in)]
         (let [obj (unpack messages)
               byte-count (count (pack obj))
               other-messages (drop byte-count messages)]
-          (>! chan obj)
+          (if-let [new-count (get obj "Seq")]
+            (update-count current-count new-count outputs))
+          (>! (get outputs @current-count) obj)
           (if (> (count other-messages) 0)
             (recur other-messages)
-            (recur (read-socket in))))))
-    chan))
+            (recur (read-socket in))))))))
 
-(defn- write-command
-  ([s command] (write-socket s (pack {"Command" command "Seq" (swap! seq-counter inc)})))
-  ([s command body] (write-socket s (-> (pack {"Command" command "Seq" (swap! seq-counter inc)})
-                                      (concat (pack body))
-                                      byte-array))))
+(defn- create-message [count command body]
+  (let [header {"Command" command "Seq" count}]
+    (if (nil? body)
+      (pack header)
+      (-> (pack header)
+          (concat (pack body))
+          byte-array))))
+
+(defn- throw-on-error [header]
+  (let [err (header "Error")]
+    (when err
+      (throw (Exception. (str "RPC Error: " err))))))
+
+(defn- attach-output-chan! [client count output]
+  (dosync (alter (:outputs client) assoc count output)))
+
+(defn- write-socket [client count b chan keep?]
+  (let [socket (:socket client)]
+    (attach-output-chan! client count {:chan chan
+                                       :keep? keep?})
+    (.write (.getOutputStream socket) b 0 (count b))))
+
+(defn- write-command [client command & {:keys [body res? keep?]}]
+   (let [chan (async/chan 2)
+         count (swap! seq-counter inc)
+         message (create-message count command body)]
+     (async/go
+       (write-socket client count message chan keep?)
+       (throw-on-error (<! chan))
+       (if res?
+         (let [res (<! chan)]
+           (when (not keep?) (async/close! chan))
+           res)
+         (when (not keep?) (async/close! chan))))))
 
 (defn- member-parser [member]
   (-> member
     (select-keys ["Name" "Status"])
     (clojure.set/rename-keys {"Name" :name "Status" :status})))
 
-(defn- handshake [{socket :socket chan :chan}]
-  (write-command socket "handshake" {"Version" 1})
-  (<!! chan))
+(defn handshake [client]
+  (<!! (write-comamnd socket "handshake" :body {"Version" 1})))
 
-(defn members [{socket :socket chan :chan}]
-  (write-command socket "members")
-  (<!! chan)
-  (let [res (<!! chan)]
-    (map member-parser (res "Members"))))
+(defn members [client]
+  (let [chan (write-command client "members" :res? true)]
+    (map member-parser ((<!! chan) "Members"))))
 
-(defn stream [{socket :socket chan :chan}]
+(defn stream [client]
   (write-command socket "stream" {"Type" "*"})
-  (<!! chan)
+  (throw-on-error (<!! chan))
   (let [event-chan (async/chan 10)]
     (async/go
-      (loop [header (<! chan)
-             event (<! chan)]
-        (>! event-chan {:event (event "Event") :member (-> (event "Members")
-                                                         first
-                                                         member-parser)})
-        (recur (<! chan)
-               (<! chan))))
+      (let [chan (write-command client "stream" :body {"Type" "*"} :res? true :keep? true)]
+        (loop [header (<! chan)
+               event (<! chan)]
+          (throw-on-error header)
+          (>! event-chan {:event (event "Event") :member (-> (event "Members")
+                                                             first
+                                                             member-parser)})
+          (recur (<! chan)
+                 (<! chan)))))
     event-chan))
 
 (defn connect [host port]
   (let [socket (Socket. host port)
         client {:socket socket
-                :chan (read-messages socket)}]
+                :outputs (ref {})}]
+    (read-messages client)
     (handshake client)
     client))
